772f3842a250af181734373236154418630aeba0
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-19 13:37:14 -0800

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,22 +10,32 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *cur_node, *temp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
 	cur_node = (*list)->next;
 
 	while(cur_node)
 	{
 		temp = cur_node->next;
 
-		while (cur_node->n < cur_node->prev->n)
+		while (cur_node->prev != NULL && cur_node->n < cur_node->prev->n)
 		{
 			cur_node->prev->next = cur_node->next;
-			cur_node->next->prev = cur_node->prev->prev;
+			
+			if (cur_node->next != NULL)
+				cur_node->next->prev = cur_node->prev;
 
 			cur_node->next = cur_node->prev;
 			cur_node->prev = cur_node->prev->prev;
 
-			cur_node->prev->next = cur_node;
-			cur_node->next->prev = cur_node;
+			if (cur_node->prev != NULL)
+				cur_node->prev->next = cur_node;
+			else
+				*list = cur_node;
+			if (cur_node->next != NULL)
+				cur_node->next->prev = cur_node;
+			
 			print_list(*list);
 		}
 		cur_node = temp;
