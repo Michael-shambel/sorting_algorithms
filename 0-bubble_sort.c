b44807547bd9d4ab22320f5ec30b11f5e9450e13
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-19 13:25:30 -0800

    task 0 ewstore

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swap;
 
 	for (i = 0; i < size; i++)
 	{
-		swap = 0;
 		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,11 +20,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swap = 1;
+
 				print_array(array, size);
 			}
 		}
-		if (!swap)
-			break;
 	}
 }
